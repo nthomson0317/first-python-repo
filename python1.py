b18819a262e86dad3b3a4bf2925a2dfaad995a65
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-20 16:37:06 -0400

    more

--- python1.py
+++ python1.py
@@ -14,7 +14,16 @@ print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
 print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
 
 
+##.format() method
+
 result = 100/777
 
 print("The result was {result}".format(result=result))
-print ("The result was {r:1.3}".format(r=result))
+print ("The result was {r:1.3f}".format(r=result))
+
+
+## string literal/ f strings/ formatted string literals
+
+name = "jose"
+
+print(f'Hello, his name is {name}')
