10eb0314f608ec41437efd1534fa56198f4d7327
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-20 16:27:15 -0400

    string methods

--- python1.py
+++ python1.py
@@ -5,4 +5,6 @@ print(20 % 2)
 
 print(2 ** 3)
 
-print('This is a string {}'.format('INSERTED'))
+print('This is a string {}'.format('{a:b}'))
+
+print('The {} {} {}'.format('fox', 'brown', 'quick'))
