51ebe73b7ae8fcfc0712205c27977300ab60be40
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-21 18:44:17 -0400

    dictionary

--- python1.py
+++ python1.py
@@ -88,3 +88,12 @@ my_sorted_list = new_list
 num_list.reverse()
 print(num_list)
 
+## Dictionaries
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+print(my_dict['key1'])
+
+prices_lookup = {'apple': 2.99, 'oranges': 1.99, 'milk': 5.80}
+
+print(prices_lookup['apple'])
