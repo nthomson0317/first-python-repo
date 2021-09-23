bee208e834941ace569de43a02a65426b67f1501
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-23 18:30:05 -0400

    tricky nest

--- python1.py
+++ python1.py
@@ -185,3 +185,7 @@ print(d['k1']['k2'])
 
 d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 print(d['k1'][0]['nest_key'][1][0])
+
+
+d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
+print(d['k1'][2]['k2'][1]['tough'][2][0])
