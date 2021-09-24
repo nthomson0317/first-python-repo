ae92b81169a99769e06a061ebfa7b72089df14c1
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-24 17:09:31 -0400

    comparison operators

--- python1.py
+++ python1.py
@@ -192,3 +192,24 @@ print(d['k1'][0]['nest_key'][1][0])
 
 d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
 print(d['k1'][2]['k2'][1]['tough'][2][0])
+
+
+######### Python comparison operators
+
+2 == 2
+#==>True
+
+2 == 1
+#==>False
+
+'hello' == 'bye'
+#==>False
+
+'Bye' == 'bye'
+#==>False
+
+'2' == 2
+#==>False
+
+2.0 == 2
+#==>True
