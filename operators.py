6a7a111b44de678c1dddeabaf1ccd85d60c30964
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-26 11:51:52 -0400

    range

---
+++ operators.py
@@ -0,0 +1,14 @@
+mylist = [1,2,3]
+
+for num in range(10):
+    print(num)
+#==> prints 0 through 9
+
+for num in range(3,10):
+    print(num)
+#==> prints 3 through 9
+
+for num in range(0,10,2):
+    print(num)
+    #==> prints 0 2 4 6 8
+    
