a497413a612aa8c7c278baafece41c24f6f1533d
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-28 17:28:14 -0400

    methods

---
+++ methods_and_functions.py
@@ -0,0 +1,16 @@
+### Methods
+### functions that are built into objects
+
+mylist = [1,2,3]
+
+mylist.append(4)
+#==> [1,2,3,4]
+
+mylist.pop()
+#==> 4
+print(mylist)
+#==> [1,2,3]
+
+help(mylist.insert)
+#==> insert(index, object, /) method of builtins.list instance
+    #==> Insert object before index.
