47821851aa8ed3fa56952a158295960de6760f1c
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-29 17:54:51 -0400

    star args

--- args_and_kwargs.py
+++ args_and_kwargs.py
@@ -1,9 +1,19 @@
 #### *args and **kwargs
 
-def myfunc(a,b):
+def myfunc(a,b,c=0,d=0,e=0):
     #Returns 5% of the sum of a and b
-    return sum((a,b)) * 0.05
+    return sum((a,b,c,d,e)) * 0.05
 
-result = myfunc(40,60)
+result = myfunc(40,60,100,100,3)
 print(result)
+#==>15.15
 
+################################################################
+
+def myfunctwo(*args):
+    #Returns 5% of the sum of a and b
+    return sum((args)) * 0.05
+
+result = myfunctwo(1,2,3,4,5,6,100,200,300,400,500,978)
+print(result)
+#==>124.95
