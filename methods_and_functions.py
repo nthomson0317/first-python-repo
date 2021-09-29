4ff86c525e21564a50d0ded6812a3ca17e04ca55
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-29 17:38:52 -0400

    cup game

--- methods_and_functions.py
+++ methods_and_functions.py
@@ -151,3 +151,42 @@ from random import shuffle
 
 shuffle(example)
 print(example)
+
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+result = shuffle_list(example)
+
+mylist = [' ','O',' ']
+
+def player_guess():
+    guess = ''
+    while guess not in ['0','1','2']:
+        guess = input("Pick a number: 0, 1, or 2")
+
+    return int(guess)
+
+    
+
+# my_index = player_guess()
+
+
+
+def check_guess(mylist,guess):
+    if mylist[guess] == 'O':
+        print("Correct")
+    else:
+        print("Wrong guess!")
+        print(mylist)
+
+#INITIAL LIST
+mylist = [' ','O',' ']
+#SHUFFLE LIST
+mixed_up_list = shuffle_list(mylist)
+#USER GUESS
+guess = player_guess()
+#CHECK GUESS
+check_guess(mixed_up_list,guess)
+
