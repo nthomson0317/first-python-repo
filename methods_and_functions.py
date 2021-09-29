8856841f66fbcdbdf734ac11f62a57e4da28f26e
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-29 17:16:22 -0400

    more tuples

--- methods_and_functions.py
+++ methods_and_functions.py
@@ -106,3 +106,15 @@ for item in stock_prices:
 #==>('APPL', 200)
 #==>('GOOG', 400)
 #==>('MSFT', 100)
+
+for ticker,price in stock_prices:
+    print(ticker)
+#==>APPL
+#==>GOOG
+#==>MSFT
+
+for ticker,price in stock_prices:
+    print(price+(0.1*price))
+#==> 220.0
+#==> 440.0
+#==> 110.0
