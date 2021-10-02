7d4b2c27740568968887b4f6d878a7a4da8dc71c
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-10-02 10:42:35 -0400

    sleeker solution to find 33

--- function_practice.py
+++ function_practice.py
@@ -99,16 +99,13 @@ print(almost_there(209))
 
 def has_33(nums):
     for i in range(0, len(nums)-1):
-     if nums[i] == 3 and nums[i] == nums[i+1]:
-        return True
-    else:
-        return False
+    #  if nums[i] == 3 and nums[i] == nums[i+1]:
+        if nums[i:i+2] == [3,3]:
+            return True
+
+    return False
 
 
 print(has_33([1,3,3]))
 print(has_33([1,3,1,3]))
 print(has_33([3,1,3]))
-
-
-    #             indexes.append(nums.index(i))
-    # return indexes
