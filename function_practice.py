ed87943eb484b55033de2b061d240dc082e02ff9
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-10-08 15:40:57 -0400

    figured out spygame

--- function_practice.py
+++ function_practice.py
@@ -167,8 +167,7 @@ def spy_game(nums):
       for i in range(0, len(nums)-1):
           if nums[i:i+3] == [0,0,7]:
             return True
-          else:
-            return False
+      return False
         
 
 print('Spy Game')
