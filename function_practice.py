5c923b7eab05c236610ea1248ee9fb1c0a35884b
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-10-11 17:05:54 -0400

    blackjack

--- function_practice.py
+++ function_practice.py
@@ -126,7 +126,7 @@ print(paper_doll('Mississippi'))
 def blackjack(a,b,c):
     if (a + b + c) <= 21:
         return (a + b + c)
-    elif (a + b + c) > 21 and (a == 11 or b == 11 or c == 11):
+    elif (a + b + c) <= 31 and (a == 11 or b == 11 or c == 11):
         return ((a + b + c) - 10)
     else:
         return 'BUST'
