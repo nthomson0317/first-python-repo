8c52fe66c6d0a6aac800d37b1197c6e82ffad01b
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-10-18 17:56:21 -0400

    filter

--- map_filter.py
+++ map_filter.py
@@ -1,3 +1,4 @@
+#### MAP
 def square(num):
     return num**2
 
@@ -21,3 +22,14 @@ print(list(map(splicer,names)))
 
 for name in map(splicer,names):
     print(name)
+
+
+    ##### FILTER
+
+def check_even(num):
+    return num%2 == 0
+
+mynums = [1,2,3,4,5,6]
+
+for n in filter(check_even,mynums):
+    print(n)
