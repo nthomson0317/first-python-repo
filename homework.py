2f27f7a7403a3ce4d2cc849ba58c98e1248d4afe
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-10-20 16:47:46 -0400

    num in range

--- homework.py
+++ homework.py
@@ -2,6 +2,21 @@
 import math
 
 def vol(rad):
-  return  (4/3) * (math.pi * (rad**3))
+  return  (4/3) * math.pi * (rad**3)
 
 print(vol(2))
+
+
+##Write a function that checks whether a number is in a given range (inclusive of high and low)
+
+def ran_check(num,low,high):
+    if num in range(low,high):
+            return True
+    else:
+            return False
+
+print(ran_check(5,2,7))
+print(ran_check(99,2,7))
+print(ran_check(500,2,7000))
+print(ran_check(15,2,7))
+print(ran_check(55,2,70))
